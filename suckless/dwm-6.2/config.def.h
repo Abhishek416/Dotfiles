35f8cffe57b7bcbd84379472187160f705abac75
Abhishek <abhisheksharma9465@gmail.com>
2021-09-09 12:42:55 +0530

    updated dwm

--- suckless/dwm-6.2/config.def.h
+++ suckless/dwm-6.2/config.def.h
@@ -13,7 +13,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "Ubuntu Mono Nerd Font:size=10" };
+static const char *fonts[]          = { "monospace:size=10",
+																				"Hack Nerd Font:pixelsize=12",
+																				"Source Code Pro Regular:size=10",
+																			};
 static const char dmenufont[]       = "monospace:size=10";
 
 static const char col_1[]  = "#282c34"; /* background color of bar */
